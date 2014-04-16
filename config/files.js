af5ca0cd4420285fcd368c9ef1b5d2966c70e0c9
Nathan Witmer <nathan@zerowidth.com>
2014-04-15 22:59:42 -0600

    Updated to latest lineman

--- config/files.js
+++ config/files.js
@@ -1,31 +1,37 @@
-/* Exports an object that defines
- *  all of the paths & globs that the project
- *  is concerned with.
+/* Exports a function which returns an object that overrides the default &
+ *   plugin file patterns (used widely through the app configuration)
  *
- * The "configure" task will require this file and
- *  then re-initialize the grunt config such that
- *  directives like <config:files.js.app> will work
- *  regardless of the point you're at in the build
- *  lifecycle.
+ * To see the default definitions for Lineman's file paths and globs, see:
  *
- * To see the default definitions for all of Lineman's file paths and globs, look at:
- * https://github.com/testdouble/lineman/blob/master/config/files.coffee
+ *   - https://github.com/linemanjs/lineman/blob/master/config/files.coffee
  */
+module.exports = function(lineman) {
+  //Override file patterns here
+  return {
+    coffee: {
+      app: [
+        "app/js/vector.coffee",
+        "app/js/utils.coffee",
+        "app/js/quadtree.coffee",
+        "app/js/display.coffee",
+        "app/js/world.coffee",
+        "app/js/geometry.coffee",
+        "app/js/contact.coffee",
+        "app/js/polygonal_body.coffee",
+        "app/js/bodies.coffee",
+        "app/js/particle.coffee",
+        "app/js/main.coffee"
+      ]
+    }
 
-module.exports = require(process.env['LINEMAN_MAIN']).config.extend('files', {
-  coffee: {
-    app: [
-      "app/js/vector.coffee",
-      "app/js/utils.coffee",
-      "app/js/quadtree.coffee",
-      "app/js/display.coffee",
-      "app/js/world.coffee",
-      "app/js/geometry.coffee",
-      "app/js/contact.coffee",
-      "app/js/polygonal_body.coffee",
-      "app/js/bodies.coffee",
-      "app/js/particle.coffee",
-      "app/js/main.coffee"
-    ]
-  }
-});
+    // As an example, to override the file patterns for
+    // the order in which to load third party JS libs:
+    //
+    // js: {
+    //   vendor: [
+    //     "vendor/js/underscore.js",
+    //     "vendor/js/**/*.js"
+    //   ]
+    // }
+  };
+};
